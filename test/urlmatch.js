38aee3cacee551937eb52c0350192abd623f6fa3
Marcin Trepa <marcin.trepa@pragmaticcoders.com>
2019-06-25 15:08:54 +0200

    Allow websocket protocols (#3)
    * Allow websocket protocols
    
    * Edit tests cases
    
    * Websockets protocols as an option

--- test/urlmatch.js
+++ test/urlmatch.js
@@ -73,6 +73,11 @@ const noTldValidationFixtures = [
   'https://dc1esbprd01:5042/en/mdm-profitcenterorg-system-api/api/*',
 ];
 
+const webSocketsFixtures = [
+  'ws://example.com',
+  'wss://example.com',
+];
+
 for (const x of exactFixtures) {
   tap.ok(regex().test(x), x);
 }
@@ -83,4 +88,8 @@ for (const x of notExactFixtures) {
 
 for (const x of noTldValidationFixtures) {
   tap.ok(regex({tld: false}).test(x), x);
+}
+
+for (const x of webSocketsFixtures) {
+  tap.ok(regex({allowWebSockets: true}).test(x), x);
 }
