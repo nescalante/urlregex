e6dc85d6aacc92d61f8da0d85faa56e512aed13d
Ben <beale.ben@gmail.com>
2020-08-09 11:09:50 -0400

    Added opt for punycode validation. Also made some minor regex tweaks. Passing all of notmatch and only failing 2 of urlmatch.

--- index.js
+++ index.js
@@ -3,16 +3,18 @@
 module.exports = function (opts) {
   var exact = (opts && opts.exact !== undefined) ? opts.exact : true;
   var tldvalidation = (opts && opts.tld !== undefined) ? opts.tld : true;
+  var punyCodeValidation = (opts && opts.puny !== undefined) ? opts.puny : false;
   var allowWebSockets = (opts && opts.allowWebSockets !== undefined) ? opts.allowWebSockets : false;
   var ip = '(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])(?:\\.(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])){3}';
   var protocol = allowWebSockets ? '(?:(http|ws)(s?)\:\/\/)?' : '(?:http(s?)\:\/\/)?';
-  var auth = '([\\-;:&=\\+\\$,\\w]+@)?';
-  var host = '(?:(?:[a-z\\u00a1-\\uffff0-9_]-?)?[a-z\\u00a1-\\uffff0-9]+)';
-  var domain = '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-?)?[a-z\\u00a1-\\uffff0-9]+)?';
-  var tld = tldvalidation ? '(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))\\.?' : '';
+  var auth = '(?:(?:[A-Za-z\\u00a1-\\uffff0-9][-_]*:?)+@)?';
+  var host = '(?:(?:[A-Za-z\\u00a1-\\uffff0-9][-_]*)?[A-Za-z\\u00a1-\\uffff0-9]+[_]*)';
+  var domain = '(?:\\.(?:[\\u00a1-\\uffffA-Za-z0-9][-_]*)?[\\u00a1-\\uffffA-Za-z0-9]+[_]*)*';
+  var tld = tldvalidation ? '(?:\\.(?:[A-Za-z\\u00a1-\\uffff]{2,}))\\.?' : '';
   var port = '(?::\\d{2,5})?';
-  var path = '(?:[/?#][^\\s"]*)?';
-  var regex = '(?:' + protocol + '|www\\.)' + auth + '(?:localhost|' + ip + '|' + host + domain + tld + ')' + port + path;
+  var path = '(?:([\\/?#]|xn--)[^\\s"]*)?';
+  var puny = punyCodeValidation ? '(?:^(?=.{1,64}$).*)' : '';
+  var regex = puny + '(?:' + protocol + '|www\\.)' + auth + '(?:localhost|' + ip + '|' + host + domain + tld + ')' + port + path;
 
   return exact ? new RegExp('(?:^' + regex + '$)', 'i') : new RegExp(regex, 'ig');
 };
